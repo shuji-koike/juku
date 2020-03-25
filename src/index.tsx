acae8c9d0aa120eb216266caf4722ad4b3c4b602
Shuji Koike <shuji.koike@me.com>
2020-03-25 18:13:50 +0900

    add sandbox.js

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import "./sandbox.css";
 
 ReactDOM.render(<h1>Juku!</h1>, document.querySelector("body > main"));
