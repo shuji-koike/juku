974c244276467e478f0eac30bb2747c439bd4adc
Shuji Koike <shuji.koike@me.com>
2020-04-07 18:17:31 +0900

    Update array-map.js

--- practice/js/array-map.js
+++ practice/js/array-map.js
@@ -1,9 +1,15 @@
 //`Array#map`を使って配列の全ての要素を2倍した配列を作る処理を実装せよ
 console.log([1, 2, 3]);
-// > [2, 4, 6]
+// [2, 4, 6]
 
 //`for`文を使って`Array#map`を実装せよ
 function map() {}
+
+console.log(map([1, 2, 3], e => e * 2));
+// [2, 4, 6]
+
+console.log(map(["a", "b", "c"], e => "(" + e + ")"));
+// ["(a)", "(b)", "(c)"]
 
 export function test() {
   const arr = [1, 2, 3];
